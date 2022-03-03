7b736a43ec62c9f21c0aa8a7d3d052a6c252f62d
Maksym Salov <maksimsalov8@gmail.com>
2022-03-03 14:42:20 +0200

    create block about us with responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-var swiper = new Swiper(".courses-slider-container", {});
+
+
+
+// var swiper = new Swiper(".courses-slider-container", {});
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// const swiper = new Swiper('.swiper', {
+//
+//     // Optional parameters
+//     speed: 400,
+//     // direction: 'vertical',
+//     loop: true,
+//     // slidesPerView: 1,
+//     spaceBetween: 10,
+//     // Responsive breakpoints
+//     breakpoints: {
+//         // when window width is >= 320px
+//         320: {
+//             slidesPerView: 1,
+//             spaceBetween: 20
+//         },
+//         // when window width is >= 480px
+//         // 480: {
+//         //     slidesPerView: 3,
+//         //     spaceBetween: 30
+//         // },
+//         // when window width is >= 640px
+//         640: {
+//             slidesPerView: 2,
+//             spaceBetween: 40
+//         }
+//     },
+//
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//
+//     // And if we need scrollbar
+//     // scrollbar: {
+//     //     el: '.swiper-scrollbar',
+//     // },
+// });
+
+
+
