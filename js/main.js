8478e1eb30b355696028affad345852a9a66bebf
Maksym Salov <maksimsalov8@gmail.com>
2022-03-07 15:16:44 +0200

    add block-slider masterClass with responsive

--- js/main.js
+++ js/main.js
@@ -45,7 +45,45 @@
 //     // scrollbar: {
 //     //     el: '.swiper-scrollbar',
 //     // },
-// });
+// }
 
 
+// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+// new Swiper('.masterClass-slider',{
+//     // Optional parameters
+//     speed: 400,
+//     // direction: 'vertical',
+//     loop: true,
+//     // slidesPerView: 1,
+//     spaceBetween: 50,
+//     // Responsive breakpoints
+//     breakpoints: {
+//         // when window width is >= 320px
+//         320: {
+//             slidesPerView: 1,
+//             spaceBetween: 20
+//         },
+//         // when window width is >= 640px
+//         998: {
+//             slidesPerView: 2,
+//             spaceBetween: 40
+//         },
+//     },
+//
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// })
 
