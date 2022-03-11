cc57193641a97dea8f2c9932b10ed88a73dfa5c7
Maksym Salov <maksimsalov8@gmail.com>
2022-03-11 19:59:20 +0200

    add html plugin in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
         main: './js/main.js',
+        // wow: './node_modules/wow.js/dist/wow.min.js',
+        // swiper: './node_modules/swiper/swiper-bundle.min.js'
     },
     output: {
-        filename: './bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack landing page',
+            template: "./index.html"
+        })
+    ]
+
 }
