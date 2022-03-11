19567a72619906c19a2031fc0e98439ae2c90f0d
Maksym Salov <maksimsalov8@gmail.com>
2022-03-11 15:31:28 +0200

    init webpack and create files bundle.js and webpack.config.js

--- js/main.js
+++ js/main.js
@@ -1,89 +1,127 @@
 
+const wow = new WOW;
+wow.init();
 
+// import Swiper from './node_modules/swiper/swiper-bundle.min.js'
 
-// var swiper = new Swiper(".courses-slider-container", {});
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// const swiper = new Swiper('.swiper', {
-//
-//     // Optional parameters
-//     speed: 400,
-//     // direction: 'vertical',
-//     loop: true,
-//     // slidesPerView: 1,
-//     spaceBetween: 10,
-//     // Responsive breakpoints
-//     breakpoints: {
-//         // when window width is >= 320px
-//         320: {
-//             slidesPerView: 1,
-//             spaceBetween: 20
-//         },
-//         // when window width is >= 480px
-//         // 480: {
-//         //     slidesPerView: 3,
-//         //     spaceBetween: 30
-//         // },
-//         // when window width is >= 640px
-//         640: {
-//             slidesPerView: 2,
-//             spaceBetween: 40
-//         }
-//     },
-//
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-//
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//
-//     // And if we need scrollbar
-//     // scrollbar: {
-//     //     el: '.swiper-scrollbar',
-//     // },
-// }
+new Swiper('.swiper',{
+    // Optional parameters
+    speed: 400,
+    // direction: 'vertical',
+    loop: true,
+    // slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
 
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1200:{
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+    },
 
-// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
-// new Swiper('.masterClass-slider',{
-//     // Optional parameters
-//     speed: 400,
-//     // direction: 'vertical',
-//     loop: true,
-//     // slidesPerView: 1,
-//     spaceBetween: 50,
-//     // Responsive breakpoints
-//     breakpoints: {
-//         // when window width is >= 320px
-//         320: {
-//             slidesPerView: 1,
-//             spaceBetween: 20
-//         },
-//         // when window width is >= 640px
-//         998: {
-//             slidesPerView: 2,
-//             spaceBetween: 40
-//         },
-//     },
-//
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-//
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: '.swiper-scrollbar',
-//     },
-// })
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+
+new Swiper('.masterClass-slider',{
+    // Optional parameters
+    speed: 400,
+    // direction: 'vertical',
+    loop: true,
+    // slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+        630: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+
+new Swiper('.offers-wrap',{
+    // Optional parameters
+    speed: 400,
+    // direction: 'vertical',
+    loop: true,
+    // slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 50
+        },
+        // when window width is >= 640px
+        530: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
