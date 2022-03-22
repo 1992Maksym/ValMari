0715512fcbc8c4cce2a331b6dfb5d1ec7cec4cc3
Maksym Salov <maksimsalov8@gmail.com>
2022-03-22 13:26:14 +0200

    connected wow and swiper

---
+++ js/main.js
@@ -0,0 +1,124 @@
+var wow = new WOW;
+wow.init();
+
+new Swiper('.swiper',{
+    // Optional parameters
+    speed: 400,
+    // direction: 'vertical',
+    loop: true,
+    // slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1200:{
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+
+new Swiper('.masterClass-slider',{
+    // Optional parameters
+    speed: 400,
+    // direction: 'vertical',
+    loop: true,
+    // slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+        630: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+
+new Swiper('.offers-wrap',{
+    // Optional parameters
+    speed: 400,
+    // direction: 'vertical',
+    loop: true,
+    // slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 50
+        },
+        // when window width is >= 640px
+        530: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
